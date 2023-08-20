3bd47631d7beeea24f7517a771e8785dca86ebf3
cedric.decoster <cedric.decoster@gmail.com>
2023-08-20 15:09:07 +0200

    updated streamjsonrpc fixed test QueryInfo

---
+++ Substrate.NetApi/Model/Rpc/RuntimeDispatchInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Substrate.NetApi.Model.Rpc
+{
+    public class RuntimeDispatchInfo
+    {
+        public Weight Weight { get; set; }
+        public string @Class { get; set; }
+        public string PartialFee { get; set; }
+    }
+
+    public class Weight
+    {
+        public int Ref_time { get; set; }
+        public int Proof_size { get; set; }
+    }
+}
