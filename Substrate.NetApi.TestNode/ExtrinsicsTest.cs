feb1d8bda28033cf6f719babab82ccc109739842
cedric.decoster <cedric.decoster@gmail.com>
2023-10-23 21:16:26 +0200

    added a fail test with bob

--- Substrate.NetApi.TestNode/ExtrinsicsTest.cs
+++ Substrate.NetApi.TestNode/ExtrinsicsTest.cs
@@ -17,6 +17,7 @@ namespace Substrate.NetApi.TestNode
     {
         public MiniSecret MiniSecretAlice => new MiniSecret(Utils.HexToByteArray("0xe5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a"), ExpandMode.Ed25519);
         public Account Alice => Account.Build(KeyType.Sr25519, MiniSecretAlice.ExpandToSecret().ToBytes(), MiniSecretAlice.GetPair().Public.Key);
+        
         public MiniSecret MiniSecretBob => new MiniSecret(Utils.HexToByteArray("0x398f0c28f98885e046333d4a41c19cee4c37368a9832c6502f6cfd182e2aef89"), ExpandMode.Ed25519);
         public Account Bob => Account.Build(KeyType.Sr25519, MiniSecretBob.ExpandToSecret().ToBytes(), MiniSecretBob.GetPair().Public.Key);
 
@@ -70,15 +71,37 @@ namespace Substrate.NetApi.TestNode
         /// </summary>
         /// <returns></returns>
         [Test]
-        public async Task Extrinsic_RemarkAsync()
+        public async Task Extrinsic_RemarkTestAsync()
         {
             var method = new Method(0, "System", 0, "remark", new byte[] { 0x04, 0xFF });
 
-            var taskCompletionSource = new TaskCompletionSource<bool>();
+            var taskCompletionSource = new TaskCompletionSource<(bool, Hash)>();
             await _substrateClient.Author.SubmitAndWatchExtrinsicAsync((string subscriptionId, ExtrinsicStatus extrinsicUpdate) => Callback(subscriptionId, extrinsicUpdate, taskCompletionSource), method, Alice, _chargeType, 64, CancellationToken.None);
 
             var finished = await Task.WhenAny(taskCompletionSource.Task, Task.Delay(TimeSpan.FromMinutes(1))); // 5 minutes or any appropriate timeout
             Assert.AreEqual(taskCompletionSource.Task, finished, "Test timed out waiting for final callback");
+        }
+
+        [Test]
+        public async Task Extrinsic_FailTestAsync()
+        {
+
+            var method = new Method(0, "System", 1, "set_heap_pages", new U64(999).Encode());
+
+            var taskCompletionSource = new TaskCompletionSource<(bool, Hash)>();
+
+            var account = Bob;
+
+            var subscriptionId = await _substrateClient.Author.SubmitAndWatchExtrinsicAsync((string subscriptionId, ExtrinsicStatus extrinsicUpdate) => Callback(subscriptionId, extrinsicUpdate, taskCompletionSource), method, account, _chargeType, 64, CancellationToken.None);
+
+            var finished = await Task.WhenAny(taskCompletionSource.Task, Task.Delay(TimeSpan.FromMinutes(1))); // 5 minutes or any appropriate timeout
+            Assert.AreEqual(taskCompletionSource.Task, finished, "Test timed out waiting for final callback");
+
+            var block = await _substrateClient.Chain.GetBlockAsync((await taskCompletionSource.Task).Item2);
+
+            var accountExtrinsics = block.Block.Extrinsics.Where(p => p.Signed && p.Account.Value == account.Value);
+            Assert.AreEqual(1, accountExtrinsics.Count());
+
         }
 
         /// <summary>
@@ -87,14 +110,14 @@ namespace Substrate.NetApi.TestNode
         /// <param name="subscriptionId"></param>
         /// <param name="extrinsicUpdate"></param>
         /// <param name="taskCompletionSource"></param>
-        private static void Callback(string subscriptionId, ExtrinsicStatus extrinsicUpdate, TaskCompletionSource<bool> taskCompletionSource)
+        private static void Callback(string subscriptionId, ExtrinsicStatus extrinsicUpdate, TaskCompletionSource<(bool, Hash)> taskCompletionSource)
         {
             ActionExtrinsicUpdate(subscriptionId, extrinsicUpdate);
             if (extrinsicUpdate.ExtrinsicState == ExtrinsicState.Finalized ||
                 extrinsicUpdate.ExtrinsicState == ExtrinsicState.Dropped ||
                 extrinsicUpdate.ExtrinsicState == ExtrinsicState.Invalid)
             {
-                taskCompletionSource.SetResult(true);
+                taskCompletionSource.SetResult((true, extrinsicUpdate.Hash));
             }
         }
 
